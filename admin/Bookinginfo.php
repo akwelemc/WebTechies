68a64fa456ac67f11d0471f3e614abb7f4f8d1e3
akwelemc <juliann.mc-addy@ashesi.edu.gh>
2024-03-17 16:45:22 +0000

    admin booking info page

--- admin/Bookinginfo.php
+++ admin/Bookinginfo.php
@@ -56,7 +56,7 @@
             <div class="firstbar">
                 <div class="head-title">
                     
-                    <h2>PASSENGER INFO<img src="../images/bus10.jpg" alt="bus" style=" width:120px;height:120px"></h2>
+                    <h2><img src="../images/bus10.jpg" alt="bus" style=" width:120px;height:120px">PASSENGER INFO</h2>
                 </div>    
                 <div class="user">
                     <!-- <div class="search-box">
