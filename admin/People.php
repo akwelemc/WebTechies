b00416e31f5c061113a67f44f44b95ca9a096d74
akwelemc <juliann.mc-addy@ashesi.edu.gh>
2024-03-17 16:51:10 +0000

    admin people php

--- admin/People.php
+++ admin/People.php
@@ -76,7 +76,7 @@
             <div class="firstbar">
                 <div class="head-title">
                     
-                    <h2>PASSENGER INFO<img src="../images/bus10.jpg" alt="bus" style=" width:120px;height:120px"></h2>
+                    <h2><img src="../images/bus10.jpg" alt="bus" style=" width:120px;height:120px">PASSENGER INFO</h2>
                 </div>    
                 <div class="user">
                     <img src="../images/defaultprofile3.png" alt=""> 
