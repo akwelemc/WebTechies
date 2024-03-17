27c2a44b3035f334db2b5c09572060e934d5f66c
akwelemc <juliann.mc-addy@ashesi.edu.gh>
2024-03-17 16:53:34 +0000

    user booking page view

--- view/bookingpage.php
+++ view/bookingpage.php
@@ -66,7 +66,7 @@ session_start();
   <div class="main">
     <div class="firstbar">
       <div class="head-title">
-        <h2>BOOKINGS<img src="../images/bus10.jpg" alt="bus" style=" width:120px;height:120px"></h2>
+        <h2><img src="../images/bus10.jpg" alt="bus" style=" width:120px;height:120px">BOOKINGS</h2>
       </div>
       <div class="user">
         <!-- <div class="search-box">
